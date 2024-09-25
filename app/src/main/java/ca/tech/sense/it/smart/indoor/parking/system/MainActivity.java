19973c0e0e1a5f1908749e25b9dcc700f095c72e
Rushi Patel <rushipatel22112003@gmail.com>
2024-09-24 22:57:28 -0400

    Added on backpressed feature with alert dialog box and also added README.md.

--- app/src/main/java/ca/tech/sense/it/smart/indoor/parking/system/MainActivity.java
+++ app/src/main/java/ca/tech/sense/it/smart/indoor/parking/system/MainActivity.java
@@ -1,7 +1,10 @@
 package ca.tech.sense.it.smart.indoor.parking.system;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import androidx.activity.EdgeToEdge;
+import androidx.activity.OnBackPressedCallback;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -19,5 +22,35 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
+        // Handling back button press using OnBackPressedDispatcher
+        OnBackPressedCallback callback = new OnBackPressedCallback(true) {
+            @Override
+            public void handleOnBackPressed() {
+                // Custom back button logic here, such as showing a dialog
+                new AlertDialog.Builder(MainActivity.this)
+                        .setMessage(R.string.are_you_sure_you_want_to_exit)
+                        .setCancelable(false)
+                        .setTitle(R.string.leaving)
+                        .setIcon(R.drawable.alert)
+                        .setPositiveButton(R.string.yes, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                finish();  // Close the activity
+                            }
+                        })
+                        .setNegativeButton(R.string.no, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();  // Stay in the app
+                            }
+                        })
+                        .show();
+            }
+
+        };
+        getOnBackPressedDispatcher().addCallback(this, callback);
     }
+
 }
