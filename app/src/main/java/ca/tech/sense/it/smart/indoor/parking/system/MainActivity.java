a547601901e130c7e866c7701cc733047c5b668e
NisargJoshi5986 <nisargjoshi1519@gmail.com>
2024-09-26 23:47:31 -0400

    Modified the Navigation Drawer with more menus.

--- app/src/main/java/ca/tech/sense/it/smart/indoor/parking/system/MainActivity.java
+++ app/src/main/java/ca/tech/sense/it/smart/indoor/parking/system/MainActivity.java
@@ -10,6 +10,7 @@ import android.annotation.SuppressLint;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.activity.OnBackPressedCallback;
@@ -19,6 +20,7 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
+import androidx.core.view.GravityCompat;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
@@ -29,14 +31,23 @@ import com.google.android.material.navigation.NavigationView;public class MainAc
 
     // Declare a BottomNavigationView
     BottomNavigationView bottomNavigationView;
-
     DrawerLayout drawerLayout;
+    Toolbar toolbar;
 
     // Create instances of each fragment
     Home firstFragment = new Home();
     Park secondFragment = new Park();
     Activity thirdFragment = new Activity();
-    MyAccount fourthFragment = new MyAccount();
+
+    // Create instances of each fragment for navigation drawer
+    AccountFragment fourthFragment = new AccountFragment();
+    NotificationsFragment notificationsFragment = new NotificationsFragment();
+    SettingsFragment settingsFragment = new SettingsFragment();
+    HelpFragment helpFragment = new HelpFragment();
+    PrivatePolicyFragment privatePolicyFragment = new PrivatePolicyFragment();
+    TermsOfUseFragment termsOfUseFragment = new TermsOfUseFragment();
+    RateUsFragment rateUsFragment = new RateUsFragment();
+
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -44,15 +55,36 @@ import com.google.android.material.navigation.NavigationView;public class MainAc
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toolbar toolbar = findViewById(R.id.nisToolbar); //Ignore red line errors
+        toolbar = findViewById(R.id.nisToolbar); //Ignore red line errors
         setSupportActionBar(toolbar);
+
         drawerLayout = findViewById(R.id.main);
         NavigationView navigationView = findViewById(R.id.nis_nav_view);
         navigationView.setNavigationItemSelectedListener((NavigationView.OnNavigationItemSelectedListener) this);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.drawer_open,
-                R.string.drawer_close);
-        drawerLayout.addDrawerListener(toggle);
-        toggle.syncState();
+
+        // Add DrawerListener to control toolbar visibility
+        drawerLayout.addDrawerListener(new DrawerLayout.DrawerListener() {
+            @Override
+            public void onDrawerSlide(@NonNull View drawerView, float slideOffset) {
+                // Do nothing
+            }
+
+            @Override
+            public void onDrawerOpened(@NonNull View drawerView) {
+                toolbar.setVisibility(View.VISIBLE); // Hide the toolbar
+            }
+
+            @Override
+            public void onDrawerClosed(@NonNull View drawerView) {
+                toolbar.setVisibility(View.VISIBLE); // Show the toolbar
+            }
+
+            @Override
+            public void onDrawerStateChanged(int newState) {
+                // Do nothing
+            }
+        });
+
 
         // Find the BottomNavigationView in the layout
         bottomNavigationView = findViewById(R.id.bottom_navigation);
@@ -123,10 +155,39 @@ import com.google.android.material.navigation.NavigationView;public class MainAc
                     .commit();
             return true;
         } else if (itemId == R.id.navigation_account) {
-            getSupportFragmentManager()
-                    .beginTransaction()
-                    .replace(R.id.flFragment, fourthFragment)
-                    .commit();
+            drawerLayout.openDrawer(GravityCompat.START);
+            return true;
+        } else if (itemId == R.id.nav_manage_account) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.flFragment,fourthFragment).commit();
+            drawerLayout.closeDrawer(GravityCompat.START);
+            return true;
+        }else if (itemId == R.id.nav_notifications) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, notificationsFragment).commit();
+            drawerLayout.closeDrawer(GravityCompat.START);
+            return true;
+        } else if (itemId == R.id.nav_settings) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, settingsFragment).commit();
+            drawerLayout.closeDrawer(GravityCompat.START);
+            return true;
+        } else if (itemId == R.id.nav_help) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, helpFragment).commit();
+            drawerLayout.closeDrawer(GravityCompat.START);
+            return true;
+        } else if (itemId == R.id.nav_private_policy) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, privatePolicyFragment).commit();
+            drawerLayout.closeDrawer(GravityCompat.START);
+            return true;
+        } else if (itemId == R.id.nav_terms_of_use) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, termsOfUseFragment).commit();
+            drawerLayout.closeDrawer(GravityCompat.START);
+            return true;
+        } else if (itemId == R.id.rate_us) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, rateUsFragment).commit();
+            drawerLayout.closeDrawer(GravityCompat.START);
+            return true;
+        } else if (itemId == R.id.nav_logout) {
+            // Handle logout
+            drawerLayout.closeDrawer(GravityCompat.START);
             return true;
         }
         return false;  // Return false if no match is found
--- app/src/main/java/ca/tech/sense/it/smart/indoor/parking/system/PrivatePolicyFragment.java
+++ app/src/main/java/ca/tech/sense/it/smart/indoor/parking/system/PrivatePolicyFragment.java
@@ -1,18 +1,18 @@
-/*Name: Kunal Dhiman, StudentID: N01540952,  section number: RCB
-  Name: Raghav Sharma, StudentID: N01537255,  section number: RCB
-  Name: NisargKumar Pareshbhai Joshi, StudentID: N01545986,  section number: RCB
-  Name: Rushi Manojkumar Patel, StudentID: N01539144, section number: RCB
- */
-
 package ca.tech.sense.it.smart.indoor.parking.system;
 
 import android.os.Bundle;
+
 import androidx.fragment.app.Fragment;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class MyAccount extends Fragment {
+public class PrivatePolicyFragment extends Fragment {
+
+    public PrivatePolicyFragment() {
+        // Required empty public constructor
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -23,6 +23,6 @@ public class MyAccount extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_my_account, container, false);
+        return inflater.inflate(R.layout.fragment_private_policy, container, false);
     }
 }
