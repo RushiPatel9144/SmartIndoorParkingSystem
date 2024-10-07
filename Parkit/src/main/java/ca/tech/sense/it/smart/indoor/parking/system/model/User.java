b9a1b197a360df477bb2dc7e8457a30f777a44c6
RaghavSharma7255 <raghavsharma14523@gmail.com>
2024-10-07 15:23:57 -0400

    Created a User Class

---
+++ Parkit/src/main/java/ca/tech/sense/it/smart/indoor/parking/system/model/User.java
@@ -0,0 +1,61 @@
+package ca.tech.sense.it.smart.indoor.parking.system.model;
+
+public class User {
+    private String uid;       // Unique user ID
+    private String firstName;      // User's name
+    private String lastName;
+    private String email;     // User's email address
+    private String phone;      // User's phone number
+
+    public User() {
+    }
+
+    public User(String uid, String firstName, String lastName, String email, String phone) {
+        this.uid = uid;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phone = phone;
+    }
+
+    // Getters and setters
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
