362769df526801125fa00447060628b4f160a745
RushiPatel9144 <p.rushi2218@gmail.com>
2024-10-08 01:13:43 -0400

    login functinality added, with signup and logout, users can be seen on firebase dashboard.Also changed the sdk to 17 and gradle to 8.7 , because of compactibilty issues with Firebase Auth dependency

--- Parkit/src/main/java/ca/tech/sense/it/smart/indoor/parking/system/MainActivity.java
+++ Parkit/src/main/java/ca/tech/sense/it/smart/indoor/parking/system/MainActivity.java
@@ -8,15 +8,17 @@ package ca.tech.sense.it.smart.indoor.parking.system;
 
 import android.annotation.SuppressLint;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.activity.OnBackPressedCallback;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
 import androidx.core.view.GravityCompat;
@@ -27,7 +29,10 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
+import ca.tech.sense.it.smart.indoor.parking.system.launcherActivity.LoginActivity;
 import ca.tech.sense.it.smart.indoor.parking.system.ui.bottomNav.Activity;
 import ca.tech.sense.it.smart.indoor.parking.system.ui.bottomNav.Home;
 import ca.tech.sense.it.smart.indoor.parking.system.ui.bottomNav.Park;
@@ -40,6 +45,13 @@ import ca.tech.sense.it.smart.indoor.parking.system.ui.navDrawer.SettingsFragmen
 import ca.tech.sense.it.smart.indoor.parking.system.ui.navDrawer.TermsOfUseFragment;
 
 public class MainActivity extends MenuHandler implements BottomNavigationView.OnItemSelectedListener, NavigationView.OnNavigationItemSelectedListener {
+
+
+    //temporary- for testing the frebase login and logout functionality
+    TextView tempTv;
+    FirebaseAuth tempAuth;
+    FirebaseUser tempUser;
+    Button tempBtn;
 
     // Declare a BottomNavigationView
     BottomNavigationView bottomNavigationView;
@@ -67,10 +79,35 @@ public class MainActivity extends MenuHandler implements BottomNavigationView.On
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //temp opr
+        tempAuth = FirebaseAuth.getInstance();
+        tempBtn = findViewById(R.id.temp_logoutBtn);
+        tempTv = findViewById(R.id.temp_tv);
+        tempUser = tempAuth.getCurrentUser();
+
+        if(tempUser == null){
+            Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+            startActivity(intent);
+            finish();
+        } else {
+            tempTv.setText(tempUser.getEmail());
+        }
+
+        tempBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+
         toolbar = findViewById(R.id.nisToolbar); //Ignore red line errors
         setSupportActionBar(toolbar);
 
-        drawerLayout = findViewById(R.id.main);
+        drawerLayout = findViewById(R.id.login);
         NavigationView navigationView = findViewById(R.id.nis_nav_view);
         navigationView.setNavigationItemSelectedListener((NavigationView.OnNavigationItemSelectedListener) this);
 
@@ -107,7 +144,7 @@ public class MainActivity extends MenuHandler implements BottomNavigationView.On
 
         EdgeToEdge.enable(this);
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.login), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
--- Parkit/src/main/java/ca/tech/sense/it/smart/indoor/parking/system/launcherActivity/SignUpActivity.java
+++ Parkit/src/main/java/ca/tech/sense/it/smart/indoor/parking/system/launcherActivity/SignUpActivity.java
@@ -1,13 +1,120 @@
 package ca.tech.sense.it.smart.indoor.parking.system.launcherActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.button.MaterialButton;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import ca.tech.sense.it.smart.indoor.parking.system.MainActivity;
+import ca.tech.sense.it.smart.indoor.parking.system.R;
 
 public class SignUpActivity extends AppCompatActivity {
 
+
+
+    EditText editTextEmail,editTextPassword;
+    MaterialButton button;
+    TextView textView;
+    ProgressBar progressBar;
+    FirebaseAuth mAuth;
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){ // this will check if the user is already logged in or not, if yes then it will redirect user to main activity
+            Intent intent = new Intent(getApplicationContext(),SignUpActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    }
+
+    @SuppressLint("CutPasteId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_sign_up);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.signup), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        editTextEmail = findViewById(R.id.signup_editTextEmail);
+        editTextPassword = findViewById(R.id.signup_editTextEmail);
+        textView = findViewById(R.id.jump_to_login);
+        button = findViewById(R.id.buttonSignUp);
+        progressBar = findViewById(R.id.signup_progressBar);
+        mAuth = FirebaseAuth.getInstance();
+
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                progressBar.setVisibility(View.VISIBLE);
+                String email,password;
+                email = String.valueOf(editTextEmail.getText());
+                password = String.valueOf(editTextPassword.getText());
+                //when email is empty
+                if(TextUtils.isEmpty(email)){
+                    Toast.makeText(SignUpActivity.this, "Enter Email", Toast.LENGTH_SHORT).show();
+                    return  ;
+                }
+                //when password is empty - remainder change this to alert in post(RushiPatel)
+                if(TextUtils.isEmpty(password)){
+                    Toast.makeText(SignUpActivity.this, "Enter Password", Toast.LENGTH_SHORT).show();
+                }
+
+                mAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                progressBar.setVisibility(View.GONE);
+                                if (task.isSuccessful()) {
+                                    // If sign in succeeds, display a message to the user.
+                                    Toast.makeText(SignUpActivity.this, "Account created.",
+                                            Toast.LENGTH_SHORT).show();
+                                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                                    startActivity(intent);
+                                    finish();
+
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Toast.makeText(SignUpActivity.this, "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+            }
+        });
     }
 }
