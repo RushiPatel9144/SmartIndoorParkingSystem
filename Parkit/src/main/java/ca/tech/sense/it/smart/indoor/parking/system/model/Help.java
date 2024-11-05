9779b549730a7a9c334b609d63a92bc186a4b0c9
NisargJoshi5986 <nisargjoshi1519@gmail.com>
2024-11-05 00:14:47 -0500

    Added Some TextView fields and  EditText field in HelpFragment.

---
+++ Parkit/src/main/java/ca/tech/sense/it/smart/indoor/parking/system/model/Help.java
@@ -0,0 +1,52 @@
+package ca.tech.sense.it.smart.indoor.parking.system.model;
+
+public class Help {
+    private String name;
+    private String phone;
+    private String email;
+    private String comment;
+
+    public Help() {
+        // Default constructor required for calls to DataSnapshot.getValue(Help.class)
+    }
+
+    public Help(String name, String phone, String email, String comment) {
+        this.name = name;
+        this.phone = phone;
+        this.email = email;
+        this.comment = comment;
+    }
+
+    // Getters and setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+}
