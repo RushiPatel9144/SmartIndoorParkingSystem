877bd468e01cd023ec9bd56460c47c8c8d56dd89
NisargJoshi5986 <nisargjoshi1519@gmail.com>
2024-11-11 02:15:54 -0500

    Displayed the toast for invalid inputs.

--- Parkit/src/main/java/ca/tech/sense/it/smart/indoor/parking/system/ui/bottomNav/AccountItems/RateUsFragment.java
+++ Parkit/src/main/java/ca/tech/sense/it/smart/indoor/parking/system/ui/bottomNav/AccountItems/RateUsFragment.java
@@ -97,44 +97,54 @@ public class RateUsFragment extends Fragment {
                 String manufacturer = Build.MANUFACTURER;
                 String fullDeviceInfo = manufacturer + " " + deviceModel;
 
-                if (rating > 0 || !comment.isEmpty() || !selectedOptions.isEmpty()) {
-                    // Fetch user information from Firestore
-                    String uid = auth.getCurrentUser().getUid();
-                    db.collection("users").document(uid).get()
-                            .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
-                                @Override
-                                public void onSuccess(DocumentSnapshot documentSnapshot) {
-                                    User user = documentSnapshot.toObject(User.class);
-                                    if (user != null) {
-                                        String userName = user.getFirstName() + " " + user.getLastName();
-                                        String userEmail = user.getEmail();
-                                        String userPhone = user.getPhone();
-
-                                        // Create a new RateUs object
-                                        RateUs feedback = new RateUs(rating, comment, fullDeviceInfo, userName, userEmail, userPhone, selectedOptions);
-
-                                        // Add a new document with generated ID to the 'feedback' collection
-                                        db.collection("feedback").add(feedback)
-                                                .addOnSuccessListener(documentReference -> {
-                                                    Toast.makeText(getContext(), getString(R.string.feedback_submitted_successfully), Toast.LENGTH_SHORT).show();
-                                                })
-                                                .addOnFailureListener(e -> {
-                                                    Toast.makeText(getContext(), getString(R.string.error_submitting_feedback) + e.getMessage(), Toast.LENGTH_SHORT).show();
-                                                });
-
-                                        // Clear the inputs after submission (optional)
-                                        ratingBar.setRating(0);
-                                        feedbackComment.setText("");
-                                        clearSelectedOptions();
-                                    }
-                                }
-                            })
-                            .addOnFailureListener(e -> {
-                                Toast.makeText(getContext(), getString(R.string.error_fetching_user_info) + e.getMessage(), Toast.LENGTH_SHORT).show();
-                            });
-                } else {
-                    Toast.makeText(getContext(), getString(R.string.please_provide_a_rating_comment_or_select_an_option), Toast.LENGTH_SHORT).show();
+                // Validate inputs
+                if (rating == 0) {
+                    Toast.makeText(getContext(), "Please provide Ratings", Toast.LENGTH_SHORT).show();
+                    return;
                 }
+//                if (comment.isEmpty()) {
+//                    feedbackComment.setError("Please provide a comment");
+//                    return;
+//                }
+                if (selectedOptions.isEmpty()) {
+                    Toast.makeText(getContext(), "Please select an option", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                // Fetch user information from Firestore
+                String uid = auth.getCurrentUser().getUid();
+                db.collection("users").document(uid).get()
+                        .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                            @Override
+                            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                                User user = documentSnapshot.toObject(User.class);
+                                if (user != null) {
+                                    String userName = user.getFirstName() + " " + user.getLastName();
+                                    String userEmail = user.getEmail();
+                                    String userPhone = user.getPhone();
+
+                                    // Create a new RateUs object
+                                    RateUs feedback = new RateUs(rating, comment, fullDeviceInfo, userName, userEmail, userPhone, selectedOptions);
+
+                                    // Add a new document with generated ID to the 'feedback' collection
+                                    db.collection("feedback").add(feedback)
+                                            .addOnSuccessListener(documentReference -> {
+                                                Toast.makeText(getContext(), getString(R.string.feedback_submitted_successfully), Toast.LENGTH_SHORT).show();
+                                            })
+                                            .addOnFailureListener(e -> {
+                                                Toast.makeText(getContext(), getString(R.string.error_submitting_feedback) + e.getMessage(), Toast.LENGTH_SHORT).show();
+                                            });
+
+                                    // Clear the inputs after submission (optional)
+                                    ratingBar.setRating(0);
+                                    feedbackComment.setText("");
+                                    clearSelectedOptions();
+                                }
+                            }
+                        })
+                        .addOnFailureListener(e -> {
+                            Toast.makeText(getContext(), getString(R.string.error_fetching_user_info) + e.getMessage(), Toast.LENGTH_SHORT).show();
+                        });
             }
         });
     }
