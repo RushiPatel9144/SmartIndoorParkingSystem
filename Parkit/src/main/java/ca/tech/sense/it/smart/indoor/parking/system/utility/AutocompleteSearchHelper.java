9773cbb8d2cf87efcead5079ea24bea6688b2d15
Raghav Sharma <156632806+RaghavSharma7255@users.noreply.github.com>
2024-11-23 22:32:02 -0500

    Add Constants

--- Parkit/src/main/java/ca/tech/sense/it/smart/indoor/parking/system/utility/AutocompleteSearchHelper.java
+++ Parkit/src/main/java/ca/tech/sense/it/smart/indoor/parking/system/utility/AutocompleteSearchHelper.java
@@ -1,16 +1,13 @@
 package ca.tech.sense.it.smart.indoor.parking.system.utility;
 
+import static ca.tech.sense.it.smart.indoor.parking.system.utility.Constants.API_PLACE_SEARCH;
 import android.content.Context;
-import android.util.Log;
-
 import androidx.annotation.NonNull;
-
 import com.google.android.gms.common.api.Status;
 import com.google.android.libraries.places.api.Places;
 import com.google.android.libraries.places.api.model.Place;
 import com.google.android.libraries.places.widget.AutocompleteSupportFragment;
 import com.google.android.libraries.places.widget.listener.PlaceSelectionListener;
-
 import java.util.Arrays;
 
 public class AutocompleteSearchHelper {
@@ -23,7 +20,7 @@ public class AutocompleteSearchHelper {
     // Initialize the AutocompleteSearch with a listener for place selection
     public static void initializeAutocompleteSearch(AutocompleteSupportFragment autocompleteFragment, Context context, final PlaceSelectionCallback callback) {
         if (!Places.isInitialized()) {
-            Places.initialize(context, "AIzaSyCBb9Vk3FUhAz6Tf7ixMIk5xqu3IGlZRd0");
+            Places.initialize(context, API_PLACE_SEARCH );
         }
 
         autocompleteFragment.setPlaceFields(Arrays.asList(Place.Field.ID, Place.Field.DISPLAY_NAME, Place.Field.LOCATION, Place.Field.FORMATTED_ADDRESS));
